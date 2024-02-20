acfe51f2f9e477482b1ad3e5227d9db4ed2d53c5
Solomon Dan <160561720+Soldan3@users.noreply.github.com>
2024-02-20 04:13:04 +0100

    Create chekout.php

---
+++ chekout.php
@@ -0,0 +1,7 @@
+<p class="form-row form-row-wide validate-required validate-region" id="shipping_region_field" data-priority="6">
+
+  <select name="shipping_region" id="shipping_region" class="state_select select2-selection--single" autocomplete="address-level1" data-placeholder="" tabindex="-1" aria-hidden="true">
+        <option>Opción 01</option>
+        <option>Opción 02</option>
+  </select>
+</p>
